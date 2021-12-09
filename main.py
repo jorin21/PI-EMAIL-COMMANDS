b01f5ce4b2f747758282f878bd2f58f1d96ee469
Jorge Avila <jorgeeavila1@gmail.com>
2021-12-08 21:22:52 -0500

    Command Handler v2 + fixed siri mail issue

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ MAC = os.getenv('MACA')
 serv = os.getenv('SERV')
 SSHuser = os.getenv('SSHuser')
 SSHpass = os.getenv('SSHpass')
-auth = str('User not Authenticated')
+auth = 'User not Authenticated'
 
 # authorized users and their identities 
 users = {
@@ -28,40 +28,78 @@ users = {
 }
 
 
-# commands
-def ping(From):
-    if From in users.keys():
-        print('pong')
-    else:
-        auth
-def turnon(From):
-    if From in users.keys():
-        print('Waking PC')
-        send_magic_packet(MAC)
-def turnoff(From):
-    if From in users.keys():
-        print('Sleeping PC')
-        ssh.connect(serv, username=SSHuser, password=SSHpass)
-        ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('shutdown -s -f -t 5')
+
+
+# command handler v2
+class commandHandler:
+    def __init__(self,subject,From):
+        #defines subject and from in the class
+        self.subject = subject
+        self.From = From
+
+        #list used to check if each command ran properly
+        self.tst = []
+
+    #commands start here
+    def ping(self):
+        if self.subject == 'ping':
+            if self.From in users.keys():
+                self.tst.append(1) # appends a 1 to the list which is then compared in the run() method
+
+                print('pong')
+            else:
+                print(auth)
+
+    def turnon(self):
+        if self.subject == 'wakepc':
+            if self.From in users.keys():
+                self.tst.append(1)
+
+                print('Waking PC')
+                send_magic_packet(MAC)
         
-    else:
-        auth
-def stop(From):
-    if From in users.keys():
-        print('Stopping Crontab Updates')
-        cmd = 'ping -c 2 localhost'
-        process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        print(output)
-def restart(From):
-    'restart code here'
+    def turnoff(self):
+        if self.subject == 'sleeppc':
+            if self.From in users.keys():
+                self.tst.append(1)
 
-
-
-
-
-    
+                print('Sleeping PC')
+                ssh.connect(serv, username=SSHuser, password=SSHpass)
+                ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('shutdown -s -f -t 5')    
+            else:
+                print(auth)
         
+    def stop(self):
+        if self.subject == 'stopscr':
+            if self.From in users.keys():
+                self.tst.append(1)
+
+                print('Stopping Crontab Updates')
+                cmd = 'ping -c 2 localhost'
+                process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+                output, error = process.communicate()
+
+    def restart(self):
+        if self.subject == 'restpc':
+            self.tst.append(1)
+
+            'restart code here'
+
+    #commands end here
+    def run(self): #run function for easy addition of commands to both multipart and single part emails
+        #add commands here
+        self.ping()
+        self.turnon()
+        self.turnoff()
+        self.stop()
+        self.restart()
+
+        #list of all functions methods dunder and run methods
+        tst2 = [method for method in dir(commandHandler) if method.startswith('__') is False and method != 'run']
+
+        # compares amt of methods to those that failed
+        if len(self.tst) != len(tst2):
+            print('command not found please try again')
 
 
 #connection
@@ -98,31 +136,28 @@ if UnSeen > Read:
             From = From[0][0].split('<')
             From = From[1][:-1]
 
+            #defines cH object and passes subject,From
+            cH = commandHandler(subject,From)
+
             if msg.is_multipart():
                 for part in msg.walk():
                     c_type = part.get_content_type()
                     c_dispo = str(part.get('Content-Disposition'))
-                    
+
                     if c_type == 'text/plain' and 'attachment' not in c_dispo:
                         body = part.get_payload(decode=True).decode()
                         
-                        #command handler
-                        if subject == 'wakepc':
-                            turnon(From)
-                        elif subject == 'sleeppc':
-                            turnoff(From)
-                        elif subject == 'ping':
-                            ping(From)
-                        elif subject == 'stopscr':
-                            stop(From)
-                        elif subject == 'restartpc':
-                            restart(From)
-                        else:
-                            print('Command is not Found. Please try again!')
+                        #run commandhandler
+                        cH.run()
+            
                         
 
                     elif 'attachment' in c_dispo:
                         print('Email contains attachment, Cannot Run Commands')
+
+            else: #if not multipart
+                body = msg.get_payload(decode=True).decode()
+                cH.run()
 else:
     print("No New Messages")
     sleep(2)
